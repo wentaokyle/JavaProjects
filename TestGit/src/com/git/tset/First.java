f80686d8409ec9fb131d93b15ee459de16e1fec6
wentao <wentaokyle@163.com>
2018-04-09 15:57:33 +0800

    say yes

---
+++ TestGit/src/com/git/tset/First.java
@@ -0,0 +1,27 @@
+package com.git.tset;
+
+/**
+ * 
+ * @author wen
+ *
+ */
+public class First 
+{
+	/**
+	 * 
+	 * @param str
+	 */
+	public static void say(String str)
+	{
+		System.out.println(str);
+	}
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		say("say YES !");
+	}
+}
